049cfa5408b94eeb238705c70cb49da3e7b5701a
Anti213 <anti621@gmail.com>
2015-08-28 14:40:45 -0700

    more data files

--- Main.py
+++ Main.py
@@ -2,21 +2,35 @@
 from RiotAPI import RiotAPI #import the class
 import json
 from pprint import pprint
+import matchParser as mParser
 
 def main():
 
-	# get example data (matchReference.json)
-	with open('matchReference.json', 'w') as data_file:
-		match = json.load(data_file)
-	
-	# check if match is valid
-	
-	# make a dict with each champion
-	
-	# log each dict into its own csv file (we be using append)
+	# get data
+	api = RiotAPI('3e888957-13a3-4ba2-901c-fae3e421d998')
+	data = api.get_match(1900729148)
 
+	# write the data into a file so its not needed
+	# to access the API all the time
+	#with open('matchReference14.json', 'w') as outfile:
+		#json.dump(r, outfile, indent=4)
+	# get reference data
+	with open('itemReference.json', 'r') as data_file:
+		itemRef = json.load(data_file)
+	with open('championReference.json', 'r') as data_file:
+		championRef = json.load(data_file)
+	
+	# check if match is valid or not
+	if(mParser.matchValid(data, itemRef) == False):
+		print('ahhh')
+	else:
+		# this writes to file as well
+		mParser.makeParticipantDictionaries(data, itemRef, championRef)
 
-
+	#pprint(data['participants'][0]['participantId'])
+		# with open('corki.json', 'r') as data_file:
+		# 	monkey = json.load(data_file)
+		# pprint(monkey)
 
 if __name__ == "__main__":
 	main()
---
+++ matchParser.py
@@ -0,0 +1,165 @@
+import json
+from pprint import pprint
+import os
+
+def matchValid(data, itemRef):
+	#print(type(data['matchVersion']))
+	if '5.11' in data['matchVersion'] and checkItems(data, itemRef):
+		#print('5.11')
+		return True
+	elif '5.14' in data['matchVersion'] and checkItems(data, itemRef):
+		#print('5.14')
+		return True
+	else:
+		return False
+
+# boolean to check if there are any ap items in the game
+def checkItems(data, itemRef):
+	
+	# store all item data from the match
+	itemList = [participant['stats'][datum] 
+			 for participant in data['participants']
+			 for datum in participant['stats'] if 'item' in datum]
+
+	# see if there are any AP items. if there is, return True
+	for item in itemList:
+		if str(item) in itemRef:
+			return True
+	return False
+
+# check validity of 
+
+# make a dictionary containing the data of 
+def makeParticipantDictionaries(data, itemRef, championRef):
+
+	# 1. If there is an ap item, link it to its player
+	
+	# definitions
+	participantIds = []
+	csvHeader = []
+
+	# reference stuff
+	itemIds = ['item0', 'item1', 'item2', 
+			   'item3', 'item4', 'item5', 'item6']
+	
+	# lvl 1
+	categories = ['teams', 'matchVersion', 'queueType', 
+				  'matchCreation', 'region', 'matchDuration']
+	# cut teams, participants
+
+	# lvl 2
+	# 'participants' exceptions
+	p_Exceptions = ['masteries', 'participantId', 'runes', 'timeline']				  
+	# lvl 3
+	teamsExceptions = ['dominionVictoryScore', 'vilemawKills']
+	#'timeline' exceptions
+	t_Exceptions = ['xpDiffPerMinDeltas', 'damageTakenDiffPerMinDeltas',
+					'csDiffPerMinDeltas']
+
+	# LINKAGE
+	for participant in data['participants']:
+		for itemId in itemIds:
+			if(str(participant['stats'][itemId]) in itemRef):
+				pprint(itemRef[str(participant['stats'][itemId])])
+				participantIds.append(participant['participantId']-1)
+				break
+
+
+	# 2. LETS MAKE THE DICTIONARY NOW
+	for participantId in participantIds:
+		#print(participantId)
+		# definitions for compactness
+		count = 0
+		championDict = dict()
+		teamId = data['participants'][participantId]['teamId']
+		championName = championRef[str(data['participants']
+								   [participantId]['championId'])]
+		pprint(championName)
+		# general data/team data
+		# lvl 1 champion key
+		championDict['championName'] = championName[0]
+		championDict['championTitle'] = championName[1]
+		
+		# lvl 1 easy to clone data
+		for category in categories:
+			championDict[category] = data[category]
+		
+		# lvl 1 make sure that you get the team data that you want
+		if(teamId == 100):
+			championDict['teams'] = data['teams'][0]
+		elif(teamId == 200):
+			championDict['teams'] = data['teams'][1]
+		
+		# lvl 3 make sure to only get the specific data that you want
+		championDict['participants'] = dict()
+		for p_Categories in data['participants'][participantId]:
+			if p_Categories not in p_Exceptions:
+				championDict['participants'][p_Categories] = data['participants'][participantId][p_Categories]
+		
+		championDict['participants']['timeline'] = dict()
+		for t_Categories in data['participants'][participantId]['timeline']:
+			if t_Categories not in t_Exceptions:
+				championDict['participants']['timeline'][t_Categories] = data['participants'][participantId]['timeline'][t_Categories]
+		
+		
+		# making new directory for item names... making sure to error check!
+		championDict['participants']['stats']['itemNames'] = dict()
+		for itemName in itemIds:
+			itemId = str(championDict['participants']['stats'][itemName])
+			itemId_name = 'itemName' + itemName[-1:]
+			#print(newItemId)
+			#print(itemName)
+
+			if (itemId in itemRef):
+				#print(itemId + ': ' + itemRef[itemId])
+				championDict['participants']['stats']['itemNames'][itemId_name] = itemRef[itemId]
+			else:
+				championDict['participants']['stats']['itemNames'][itemId_name] = 'N/A'
+
+		#pprint(itemRef)
+		# writing to file
+		# writing to a specific directory
+		
+		fileName1 = championName[0].lower() + '.json'
+		directory = '/home/lenneth/Documents/RiotAPI project/parsed champion data/'
+		fileName = directory + fileName1
+		#os.path.join('/home/lenneth/Documents/RiotAPI project/parsed match data',
+		#		     fileName)
+		
+		#print(directory + fileName)
+
+		with open(fileName, 'a') as outfile:
+			if(os.stat(fileName).st_size == 0):
+				outfile.write('[\n')
+				#print(fileName + ' WAS EMPTY')
+			else:
+				#print(fileName + ' is OCCUPIEDD')
+				count = 1
+
+		if(count == 1):
+			with open(fileName, 'rb+') as filehandle:
+			    filehandle.seek(-1, os.SEEK_END)
+			    filehandle.truncate()
+			    filehandle.seek(-1, os.SEEK_END)
+			    filehandle.truncate()
+			with open(fileName, 'a') as outfile:
+				outfile.write(',\n')
+				json.dump(championDict, outfile, indent=4)
+				outfile.write('\n]')
+		else: 		
+			with open(fileName, 'a') as outfile:
+				json.dump(championDict, outfile, indent=4)
+				outfile.write('\n]')
+
+		# # validity of file
+
+
+
+		# with open(filename, 'rb+') as filehandle:
+		#     filehandle.seek(-1, os.SEEK_END)
+		#     filehandle.truncate()
+
+	return True
+
+def makeGeneralDictionary(data):
+	return True
